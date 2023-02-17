6420f6515ef0c3239a3835e7392b4ebda8e85049
Ovioisa Oisamaye Benjamin <99197115+Oisamaye1@users.noreply.github.com>
2023-02-17 15:03:47 +0300

    Delete Resume Website directory

--- Resume Website/static/app.js
+++
@@ -1,7 +0,0 @@
-let menu_toggle = document.querySelector(".menu-toggle")
-let sidebar = document.querySelector(".navigation")
-
-menu_toggle.addEventListener('click', () => {
-    menu_toggle.classList.toggle('is-active');
-    sidebar.classList.toggle('is-active');
-})
